c55bf303b37e53fca2766492f135ac745969fb28
VladosDudos1 <v.toper17@gmail.com>
2023-05-10 21:49:28 +0300

    reset password didnt tested

--- app/src/main/java/com/example/matrixapp/view/fragments/ResetPasswordFragment.kt
+++ app/src/main/java/com/example/matrixapp/view/fragments/ResetPasswordFragment.kt
@@ -1,17 +1,97 @@
 package com.example.matrixapp.view.fragments
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.example.matrixapp.R
+import com.example.matrixapp.databinding.FragmentResetPasswordBinding
+import com.example.matrixapp.viewmodel.ResetPasswordViewModel
+
 class ResetPasswordFragment : Fragment() {
 
+    private val binding: FragmentResetPasswordBinding by lazy {
+        FragmentResetPasswordBinding.inflate(
+            layoutInflater
+        )
+    }
+    private val resetPasswordViewModel: ResetPasswordViewModel by viewModels()
+    private var checkerPass = true
+    private var checkerRepeat = true
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setObservers()
+        applyClick()
+    }
+
+    private fun setObservers() {
+        resetPasswordViewModel.isEmailEntered.observe(viewLifecycleOwner){
+            if (it) visionPassword()
+            else visionEmail()
+        }
+    }
+    private fun visionEmail(){
+        binding.textView2.visibility = View.VISIBLE
+        binding.emailEditText.visibility = View.VISIBLE
+        binding.passwordEditText.visibility = View.GONE
+        binding.repeatPasswordEditText.visibility = View.GONE
+    }
+
+    private fun visionPassword(){
+        binding.textView2.visibility = View.GONE
+        binding.emailEditText.visibility = View.GONE
+        binding.passwordEditText.visibility = View.VISIBLE
+        binding.repeatPasswordEditText.visibility = View.VISIBLE
+    }
+    private fun applyClick(){
+        binding.resetBtn.setOnClickListener {
+            if (resetPasswordViewModel.isEmailEntered.value == true){
+                resetPassword()
+            }
+            else resetPasswordViewModel.isEmailEntered.value = true
+        }
+        binding.contactSupportTxt.setOnClickListener {
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_supportFragment2)
+        }
+        binding.backFrameBtn.setOnClickListener { findNavController().popBackStack() }
+        binding.showHideBtnPassword.setOnClickListener {
+            if (checkerPass) {
+                Glide.with(it).load(R.drawable.password_close_eye).into(it as ImageView)
+                binding.passwordEditText.transformationMethod = null
+                checkerPass = false
+            } else {
+                Glide.with(it).load(R.drawable.password_eye).into(it as ImageView)
+                binding.passwordEditText.transformationMethod = PasswordTransformationMethod()
+                checkerPass = true
+            }
+        }
+        binding.showHideBtnRepeat.setOnClickListener {
+            if (checkerRepeat) {
+                Glide.with(it).load(R.drawable.password_close_eye).into(it as ImageView)
+                binding.repeatPasswordEditText.transformationMethod = null
+                checkerRepeat = false
+            } else {
+                Glide.with(it).load(R.drawable.password_eye).into(it as ImageView)
+                binding.repeatPasswordEditText.transformationMethod = PasswordTransformationMethod()
+                checkerRepeat = true
+            }
+        }
+    }
+    private fun resetPassword(){
+
     }
 }
