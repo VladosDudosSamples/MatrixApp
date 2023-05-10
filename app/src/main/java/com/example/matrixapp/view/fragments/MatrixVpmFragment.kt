095ec5fa5f6ecefb5b9cd4b9d75576e8b2720295
Ἀρτεμής <ivanivanov31922@gmail.com>
2023-05-10 19:15:10 +0300

    bottom sheet added with some shitty solutions

--- app/src/main/java/com/example/matrixapp/view/fragments/MatrixVpmFragment.kt
+++ app/src/main/java/com/example/matrixapp/view/fragments/MatrixVpmFragment.kt
@@ -1,13 +1,19 @@
 package com.example.matrixapp.view.fragments
 
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.matrixapp.R
+import com.example.matrixapp.databinding.ConnectionStatisticsBottomSheetBinding
 import com.example.matrixapp.databinding.FragmentMatrixVpmBinding
+import com.example.matrixapp.utils.OnSwipeTouchListener
 import com.example.matrixapp.view.activity.DrawerActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MatrixVpmFragment : Fragment() {
 
@@ -27,6 +33,7 @@ class MatrixVpmFragment : Fragment() {
         applyCLick()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun applyCLick() {
         with(binding) {
             btnOpenDrawer.setOnClickListener {
@@ -39,6 +46,7 @@ class MatrixVpmFragment : Fragment() {
                     tvConnected.text = resources.getString(R.string.connected)
                     tvConnectionTime.text = resources.getString(R.string.time_sample)
                     cheatView.visibility = View.VISIBLE
+
                 } else {
                     imgSwitchBackground.setImageResource(R.drawable.switch_off_back)
                     roundImage.setImageResource(R.drawable.switch_round_off)
@@ -47,6 +55,28 @@ class MatrixVpmFragment : Fragment() {
                     cheatView.visibility = View.GONE
                 }
             }
+
+            cheatView.setOnTouchListener(object: OnSwipeTouchListener(requireContext()){
+                override fun onSwipeUp() {
+                    super.onSwipeUp()
+                    showBottomSheet()
+                }
+            })
         }
+    }
+
+    private fun showBottomSheet() {
+        val dialogBinding = ConnectionStatisticsBottomSheetBinding.inflate(layoutInflater)
+        val dialog = BottomSheetDialog(requireContext())
+        val view = dialogBinding.root
+        dialog.setContentView(view)
+
+        val behavior = BottomSheetBehavior.from(view.parent as View)
+        behavior.state = BottomSheetBehavior.STATE_DRAGGING
+//        behavior.peekHeight = (Resources.getSystem().displayMetrics.heightPixels * 0.07).toInt()
+        //view.minimumHeight = (Resources.getSystem().displayMetrics.heightPixels * 21.12).toInt()
+        behavior.isFitToContents = true
+
+        dialog.show()
     }
 }
