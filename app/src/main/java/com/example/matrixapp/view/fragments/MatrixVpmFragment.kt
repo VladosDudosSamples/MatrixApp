4cc1845af6835144fa11309267d737a1eb2de9ff
Ἀρτεμής <ivanivanov31922@gmail.com>
2023-05-11 16:52:19 +0300

    test

--- app/src/main/java/com/example/matrixapp/view/fragments/MatrixVpmFragment.kt
+++ app/src/main/java/com/example/matrixapp/view/fragments/MatrixVpmFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.matrixapp.R
 import com.example.matrixapp.databinding.ConnectionStatisticsBottomSheetBinding
@@ -45,38 +46,48 @@ class MatrixVpmFragment : Fragment() {
                     roundImage.setImageResource(R.drawable.switch_round_on)
                     tvConnected.text = resources.getString(R.string.connected)
                     tvConnectionTime.text = resources.getString(R.string.time_sample)
-                    cheatView.visibility = View.VISIBLE
+                    //cheatView.visibility = View.VISIBLE
+                    //showBottomSheet()
 
                 } else {
                     imgSwitchBackground.setImageResource(R.drawable.switch_off_back)
                     roundImage.setImageResource(R.drawable.switch_round_off)
                     tvConnected.text = resources.getString(R.string.no_onnected)
                     tvConnectionTime.text = ""
-                    cheatView.visibility = View.GONE
+                    //cheatView.visibility = View.GONE
                 }
             }
 
-            cheatView.setOnTouchListener(object: OnSwipeTouchListener(requireContext()){
-                override fun onSwipeUp() {
-                    super.onSwipeUp()
-                    //showBottomSheet()
-                }
-            })
+
         }
     }
 
-    private fun showBottomSheet() {
-        val dialogBinding = ConnectionStatisticsBottomSheetBinding.inflate(layoutInflater)
-        val dialog = BottomSheetDialog(requireContext())
-        val view = dialogBinding.root
-        dialog.setContentView(view)
-
-        val behavior = BottomSheetBehavior.from(view.parent as View)
-        behavior.state = BottomSheetBehavior.STATE_DRAGGING
+//    private fun showBottomSheet() {
+//        val dialogBinding = ConnectionStatisticsBottomSheetBinding.inflate(layoutInflater)
+//        val dialog = BottomSheetDialog(requireContext())
+//        val view = dialogBinding.root
+//        dialog.setContentView(view)
+//
+//        val behavior = BottomSheetBehavior.from(view.parent as View)
+//        behavior.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback(){
+//            override fun onStateChanged(bottomSheet: View, newState: Int) {
+//                if(newState == 4){
+//                   behavior.state = BottomSheetBehavior.STATE_HIDDEN
+//                }
+//                Toast.makeText(requireContext(), newState.toString(), Toast.LENGTH_SHORT).show()
+//            }
+//
+//            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+//                //Toast.makeText(requireContext(), "slide", Toast.LENGTH_SHORT).show()
+//            }
+//
+//        })
+//
+//
 //        behavior.peekHeight = (Resources.getSystem().displayMetrics.heightPixels * 0.07).toInt()
-        //view.minimumHeight = (Resources.getSystem().displayMetrics.heightPixels * 21.12).toInt()
-        behavior.isFitToContents = true
-
-        dialog.show()
-    }
+//        view.minimumHeight = (Resources.getSystem().displayMetrics.heightPixels * 0.21).toInt()
+//        behavior.isFitToContents = true
+//
+//        dialog.show()
+//    }
 }
