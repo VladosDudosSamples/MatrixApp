83322679e331a2ab3ff53b76e21d8d717cdb8048
VladosDudosSmaples <v.toper17@gmail.com>
2023-05-19 11:23:29 +0300

    support done

--- app/src/main/java/com/example/matrixapp/view/adapter/LocationAdapter.kt
+++ app/src/main/java/com/example/matrixapp/view/adapter/LocationAdapter.kt
@@ -61,13 +61,6 @@ class LocationAdapter(
                     root.strokeColor = ContextCompat.getColor(context, R.color.green)
                 }
             }
-//            if (location.isExposed) {
-//                rvCities.visibility = View.VISIBLE
-//                imageStatus.rotation = 180F
-//            } else {
-//                rvCities.visibility = View.GONE
-//                imageStatus.rotation = 0F
-//            }
             if (location.cities.size == 1) {
                 tvCity.text = location.cities[0].name
                 if (isLocked) {
@@ -109,9 +102,7 @@ class LocationAdapter(
                         rvCities.visibility = View.GONE
                     }
                 } else {
-                    if (isLocked) {
-
-                    } else {
+                    if (!isLocked) {
                         onCardClickListener(location.cities[0])
                         notifyDataSetChanged()
                         updateList(locations)
--- app/src/main/java/com/example/matrixapp/view/fragments/SupportFragment.kt
+++ app/src/main/java/com/example/matrixapp/view/fragments/SupportFragment.kt
@@ -5,14 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.matrixapp.R
+import com.example.matrixapp.databinding.FragmentSupportBinding
+import com.example.matrixapp.view.adapter.FaqAdapter
 
 class SupportFragment : Fragment() {
 
+    private val binding: FragmentSupportBinding by lazy{FragmentSupportBinding.inflate(layoutInflater)}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_support, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        applyClick()
+        setAdapter()
+    }
+
+    private fun applyClick(){
+        with(binding){
+            telegramCardView.setOnClickListener {  }
+            contactCardView.setOnClickListener {  }
+            mailCardView.setOnClickListener {  }
+        }
+    }
+
+    private fun setAdapter(){
+        with(binding){
+            val manager = LinearLayoutManager(requireContext())
+            rvFaq.layoutManager = manager
+            rvFaq.adapter = FaqAdapter(requireContext(), manager)
+        }
     }
 }
