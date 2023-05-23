0f19442e78aced5e78398d04584f6f8b73468a09
Ἀρτεμής <ivanivanov31922@gmail.com>
2023-05-23 18:09:00 +0300

    radio button changed to imageview in location recyclers

--- app/src/main/java/com/example/matrixapp/view/adapter/CityAdapter.kt
+++ app/src/main/java/com/example/matrixapp/view/adapter/CityAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.matrixapp.R
 import com.example.matrixapp.databinding.CityCardBinding
 import com.example.matrixapp.model.City
 
@@ -30,12 +31,16 @@ class CityAdapter(
     override fun onBindViewHolder(holder: CityHolder, position: Int) {
         val city = list[position]
         with(holder.binding) {
-            radioButtonConnected.isChecked = city.isSelected
+            if (city.isSelected) {
+                imageConnect.setImageResource(R.drawable.radio_back_checked)
+            } else {
+                imageConnect.setImageResource(R.drawable.radio_back)
+            }
             tvCityName.text = city.name
             if (isLocked) {
-                radioButtonConnected.visibility = View.INVISIBLE
+                imageConnect.visibility = View.INVISIBLE
             } else {
-                image.visibility = View.INVISIBLE
+                imageLock.visibility = View.INVISIBLE
                 root.setOnClickListener {
                     onClick(city)
                     notifyDataSetChanged()
